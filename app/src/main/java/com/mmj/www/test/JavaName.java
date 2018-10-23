a604c0be8ffeb44f9cbd21807964259266b7708c
Moon1 <moon0920@dreamwiz.com>
2018-10-23 13:58:40 +0900

    Initial commit

---
+++ app/src/main/java/com/mmj/www/test/JavaName.java
@@ -0,0 +1,13 @@
+package com.mmj.www.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JavaName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java_name);
+    }
+}
